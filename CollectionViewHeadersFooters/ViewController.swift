0f3d9e3a97dd716f031ddc2e1fd9277d716773a6
Fazlı Altun <altunfazl@gmail.com>
2024-02-24 16:53:44 +0300

    Initial Commit

---
+++ CollectionViewHeadersFooters/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionViewHeadersFooters
+//
+//  Created by Fazlı Altun on 24.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
