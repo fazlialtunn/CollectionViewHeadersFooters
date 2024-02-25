d53bc33f56f63a058a5feb73ce30ee5959f70584
Fazlı Altun <altunfazl@gmail.com>
2024-02-25 11:08:48 +0300

    Visualized Headers and Footers in CollectionView

---
+++ CollectionViewHeadersFooters/CustomCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCollectionViewCell.swift
+//  CollectionViewHeadersFooters
+//
+//  Created by Fazlı Altun on 24.02.2024.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ CollectionViewHeadersFooters/HeaderCollectionReusableView.swift
@@ -0,0 +1,12 @@
+//
+//  CollectionReusableView.swift
+//  CollectionViewHeadersFooters
+//
+//  Created by Fazlı Altun on 25.02.2024.
+//
+
+import UIKit
+
+class CollectionReusableView: UICollectionReusableView {
+        
+}
